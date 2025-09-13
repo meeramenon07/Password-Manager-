d9791dd118aee7987d30539acac6b049d7d14626
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2025-09-13 15:50:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,34 @@ def delete_password(user_name, user_id):
     print("Deleted the password!")
   else:
     print("unable to find password to delete!")
+
+def main():
+  print("let's create a password manager in Python!")
+  while True:
+    print("1. Add Password", "\n2. Retrieve Password", "\n3. Edit Password", "\n4. Delete Password", "\n5. Exit")
+    option = input("Select an option!")
+    if option == "1":
+      user_name = input("Please input the user name: ")
+      user_id = input("Please input the user ID : ")
+      password = getpass.getpass("Please input the password: ")
+      add_password(user_name,user_id,password)
+    elif option == "2":
+      user_name = input("Please input the user name: ")
+      user_id = input("Please  input the user ID: ")
+      retrieve_password(user_name,user_id,password)
+    elif option == "3":
+      user_name = input("Please input the user name: ")
+      user_id = input(" Please input/enter the user ID:  ")
+      new_password = getpass.getpass("Please input the new password: ")
+      edit_password(user_name,user_id,new_password)
+    elif option == "4":
+      user_name = input("Please input the user name: ")
+      user_id = input("Please  input the user ID: ")
+      delete_password(user_name,user_id )
+    elif option == "5":
+      print("Exit !")
+      break
+    else:
+      print("Please try again entering valud options! ")
+
+main()
